ba566e7e2376202827ce756e2e3599f312b58e12
eduardohc <lalohernandez93@gmail.com>
2016-04-15 17:34:33 -0500

    Working on profile settings code

--- app/src/main/java/com/example/ehernandez/asesoruanl/ProfileSettings.java
+++ app/src/main/java/com/example/ehernandez/asesoruanl/ProfileSettings.java
@@ -4,15 +4,27 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.parse.ParseUser;
 
 /**
  * Created by Eduardo on 14/04/2016.
  */
 public class ProfileSettings extends AppCompatActivity {
+
+    private EditText et_name;
+    private TextView tv_usernanme;
+    private EditText et_email;
+    private TextView tv_emailVerified;
+    private EditText et_department;
+    private EditText et_dependency;
+    ParseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +40,42 @@ public class ProfileSettings extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         tv_toolbar.setText("" + getResources().getString(R.string.profile));
 
+        et_name = (EditText) findViewById(R.id.et_profileSettings_name);
+        tv_usernanme = (TextView) findViewById(R.id.tv_profilesettings_username);
+        et_email = (EditText) findViewById(R.id.et_profileSettings_email);
+        tv_emailVerified = (TextView) findViewById(R.id.tv_profileSettings_emailVerified);
+        et_department = (EditText) findViewById(R.id.et_profileSettings_department);
+        et_dependency = (EditText) findViewById(R.id.et_profileSettings_dependency);
+
+        user = ParseUser.getCurrentUser();
+        setUserAttributes();
+    }
+
+    public void setUserAttributes(){
+
+        boolean isVerified;
+
+        et_name.setText("" + user.get("Name"));
+        tv_usernanme.setText("" + user.getUsername());
+        et_email.setText("" + user.getEmail());
+
+        isVerified = user.getBoolean("emailVerified");
+        if(isVerified){
+            tv_emailVerified.setText("" + getResources().getString(R.string.verified));
+        }else{
+            tv_emailVerified.setText("" + getResources().getString(R.string.noVerified));
+        }
+
+        et_dependency.setText("" + user.get("Dependencia"));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.save_changes, menu);
+        //btn_cancel = menu.findItem(R.id.cancel);
+        return true;
     }
 
     @Override
